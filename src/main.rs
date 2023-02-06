2557060facbb3bd7ddb20724d120470c0ca4a752
Jackson Ly <48436180+JumpyJacko@users.noreply.github.com>
2023-02-06 19:47:14 +1100

    Finished daily timetable scraper

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use axum::{
 };
 use fantoccini::{ClientBuilder, Locator};
 use serde::{Deserialize, Serialize};
-use std::net::SocketAddr;
+use std::{net::SocketAddr, collections::VecDeque};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
@@ -70,26 +70,64 @@ async fn scrape_daily_timetable(
     f.submit().await?;
 
     // TODO: Need to find some Regex
+    let mut timetable: Vec<Period> = Vec::new();
+
     let table = c.find(Locator::Css(".timetable")).await?;
     let periods = table.find_all(Locator::Css(".timetable-dayperiod")).await?;
+    let mut period_names = table.find_all(Locator::Css(".timetable-period")).await?;
 
-    for period in periods {
-        let period_text = period.text().await?;
-        let elem = period.find_all(Locator::Css("div")).await?;
-        
-        println!("{}, css: \n", period_text);
+    for period in periods.iter().zip(period_names.iter_mut()) {
+        let period_text = period.0.text().await?;
+        let period_name = period.1.text().await?;
+        let elems = period.0.find_all(Locator::Css("div")).await?;
+
+        let mut subject: String = match period_text.lines().next() {
+            Some(subject) => subject.to_owned(),
+            None => "".to_owned(),
+        };
+        let subject_short = match subject.split_whitespace().last() {
+            Some(subject) => subject.to_owned(),
+            None => "".to_owned(),
+        };
+        subject = subject.replace(&subject_short, "");
+
+        let rest_of_period_text = match period_text.lines().last() {
+            Some(subject) => subject.to_owned(),
+            None => "".to_owned(),
+        };
+
+        let room = match rest_of_period_text.split_whitespace().nth(1) {
+            Some(room) => room.to_owned(),
+            None => "".to_owned(),
+        };
+
+        let mut teacher = match rest_of_period_text.split_whitespace().nth(3) {
+            Some(teacher) => teacher.to_owned(),
+            None => "".to_owned(),
+        };
+        let teacher_1 = match rest_of_period_text.split_whitespace().nth(4) {
+            Some(teacher) => teacher.to_owned(),
+            None => "".to_owned(),
+        };
+        teacher.push(' ');
+        teacher.push_str(&teacher_1);
+
+        let mut css: VecDeque<String> = VecDeque::new();
+        for elem in elems {
+            css.push_back(elem.css_value("border-left-color").await?);
+        }
+        let colour: String = match css.pop_front() {
+            Some(colour) => colour,
+            None => "".to_owned(),
+        };
+
+        timetable.push(Period { period: period_name, subject, subject_short, room, teacher, colour });
     }
 
     c.close().await?;
 
     Ok(Json(DailyTimetable {
-        periods: vec![Period {
-            period: "1 or like recess or something".to_owned(),
-            subject: "test".to_owned(),
-            room: "test".to_owned(),
-            teacher: "test".to_owned(),
-            colour: "#test".to_owned(),
-        }],
+        periods: timetable
     }))
 }
 
@@ -97,6 +135,7 @@ async fn scrape_daily_timetable(
 struct Period {
     period: String,
     subject: String,
+    subject_short: String,
     room: String,
     teacher: String,
     colour: String,
